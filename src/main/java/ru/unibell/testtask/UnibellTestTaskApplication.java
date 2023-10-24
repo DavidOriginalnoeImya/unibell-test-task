885dc7cc88cde8a6e13bf84a66d8e21164c08e03
DavidOriginalnoeImya <abulashvili@inbox.ru>
2023-10-24 13:15:05 +0400

    init: инициализировать проект

---
+++ src/main/java/ru/unibell/testtask/UnibellTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.unibell.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibellTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnibellTestTaskApplication.class, args);
+    }
+
+}
